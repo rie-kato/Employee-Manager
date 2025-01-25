d4d35d64dc7bda1f25880b45f1ea79dc077230c1
rie kato <o8o13640696@gmail.com>
2025-01-25 09:34:53 +0900

    コメント

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -9,7 +9,6 @@
     
     <!-- CSSファイルの読み込み -->
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
     
     @yield('css')
